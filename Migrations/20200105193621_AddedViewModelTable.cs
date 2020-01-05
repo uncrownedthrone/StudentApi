632ffde53ac3fd86c4c9294e833ea6a606943f47
Justin Swiger <justin.swiger@me.com>
2020-01-05 14:40:51 -0500

    creating new progress report still broken

---
+++ Migrations/20200105193621_AddedViewModelTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace StudentApi.Migrations
+{
+    public partial class AddedViewModelTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProgressReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AttendanceIssues = table.Column<string>(nullable: true),
+                    Improvement = table.Column<string>(nullable: true),
+                    DoingWell = table.Column<string>(nullable: true),
+                    StudentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgressReports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProgressReports_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgressReports_StudentId",
+                table: "ProgressReports",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProgressReports");
+        }
+    }
+}
--- Migrations/20200105193935_AddedProgressReport2Table.Designer.cs
+++ Migrations/20200105193935_AddedProgressReport2Table.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StudentApi.Models;
@@ -9,9 +10,10 @@ using StudentApi.Models;
 namespace StudentApi.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200105193935_AddedProgressReport2Table")]
+    partial class AddedProgressReport2Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
